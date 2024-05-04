ee6b91cb28e9c1113c375cd5d4a05328c1352a69
mimasala <mytallmusic@gmail.com>
2024-05-04 16:53:03 +0200

    test

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "briones-gabriel/darcula-solid.nvim", dependencies = { "rktjmp/lush.nvim" } },
+  { "Mofiqul/dracula.nvim" },
+  { "doums/darcula" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "darcula",
+    },
+  },
+}
